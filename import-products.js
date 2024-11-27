3de1278512cb6cf1a91edfb98cc3d81d0f6604f8
Sguduru98 <sanitha.guduru@slu.edu>
2024-11-27 03:52:34 +0000

    Sanitha Lab 5

---
+++ import-products.js
@@ -0,0 +1,12 @@
+
+const db = require('../db');
+const Products = require('../products');
+
+const products = require('../data/full-products.json');
+
+;(async () => {
+  // loop over the products and create them
+  for (let i = 0; i < products.length; i++) {
+    console.log( await Products.create(products[i]));
+  }
+})()
